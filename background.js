2eb684c34c0245a2a39354abe26226802d119bcd
Ankush Goyal <54682973+ankushgoyaljmv@users.noreply.github.com>
2020-08-24 19:03:01 +0530

    Added background image

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
 
+console.log("bakground appeared");
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender);
+        if (request.greeting == "hello") {
+            console.log("Recieved from content");
+            sendResponse({ farewell: "goodbye" });
+        }
+    })
